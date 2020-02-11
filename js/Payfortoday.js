0c9d0cd50e0fd5bfee84bf7321654d2fc078305d
Monko Vitalijus <Vitalijus.Monko@swisscom.com>
2020-02-11 13:19:52 +0100

    cleancode

--- js/Payfortoday.js
+++ js/Payfortoday.js
@@ -8,15 +8,19 @@
 function HideMainPart(){
 $("#m-count").hide();
 }
+
 function PopUpShow(){
     $("#m-popup").show();
 }
+
 function PopUpHide_Main(){
     $("#m-popup").hide();
-    }         
+    }   
+          
 function inputFocus(i){
     if(i.value==i.defaultValue){ i.value=""; i.style.color="#000"; }
 }
+
 function inputBlur(i){
     if(i.value==""){ i.value=i.defaultValue; i.style.color="#888"; }
 }
